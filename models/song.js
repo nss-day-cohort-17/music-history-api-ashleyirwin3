b283fef52416cc7187c900fcc335e4481ee2f446
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-21 15:06:21 -0500

    defined model, added tableName

--- models/song.js
+++ models/song.js
@@ -1,8 +1,10 @@
 'use strict'
 
-// <Define a model using bookshelf that describes a song object, as well as 
+// <Define a model using bookshelf that describes a song object, as well as
 // static methods for getting one or all songs from the db>
-
-// When adding the 'table' property use your old friend DB Browser for SQLite to 
-// open up the musichistory db and inspect the tables. Add the appropriate table name for 
+const Song = bookshelf.Model.extend({
+  tableName = 'song'
+})
+// When adding the 'table' property use your old friend DB Browser for SQLite to
+// open up the musichistory db and inspect the tables. Add the appropriate table name for
 // interfacing with the songs collection
