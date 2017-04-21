b6d801b877435f8ce793cea4f765a9d0cb520a86
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-21 15:04:09 -0500

    included route index file

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 let app = express();
 
 // <Include the router index file>
+var routes = require('./routes/')
+
 // <Setup your routes middleware>
 // <catch any undefined routes with a 404 middleware>
-// <Handle any errors that occur in the routing with error handlers defined at the bottom of our 
+// <Handle any errors that occur in the routing with error handlers defined at the bottom of our
 // middleware stack>
