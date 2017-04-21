d7b3ada347676d9e25d3d7454bcb5c2dde73614c
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-21 15:04:48 -0500

    required bookshelf and song model, began logic to get all songs from db

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -1,5 +1,15 @@
-'use strict'
+'use strict';
 
 // <require a song model>
+const { bookshelf } = require('../db/database');
+const Song = require('../models/song')
+
 // < use model methods for getting all songs and one song then send the response back with the data>
+module.exports.getSongs = (req, res, next) => {
+  Song.getall()
+  .then( (songs) => {
+    res.status
+  })
+
+}
 // <stretch goal: methods for adding, deleting, editing a song>
